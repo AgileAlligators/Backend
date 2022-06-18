12d49cc54573df4b3d5524ef32f74ed6667cc04e
Timo Scheuermann <timo.scheuermann@gmx.de>
2022-06-18 14:00:45 +0200

    load test

--- src/load/schemas/Load.schema.ts
+++ src/load/schemas/Load.schema.ts
@@ -1,6 +1,12 @@
 import { ModelDefinition, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Location } from 'src/location/schemas/Location.schema';
-import { ApiCarrierLoad } from '../../carrier/carrier.api';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
+import { GeoJSON } from 'src/carrier/models/GeoJson.model';
+import {
+  ApiCarrierId,
+  ApiCarrierLoad,
+  ApiCarrierTimestamp,
+} from '../../carrier/carrier.api';
 
 @Schema({
   toJSON: {
@@ -8,11 +14,34 @@ import { ApiCarrierLoad } from '../../carrier/carrier.api';
     transform: (_, ret: Load) => {
       delete ret._id;
       delete ret.__v;
+
+      (ret as any).coordinates = ret.location.coordinates;
+      delete ret.location;
+
       return ret;
     },
   },
 })
-export class Load extends Location {
+export class Load extends Document {
+  @ApiCarrierId({ required: true })
+  @Prop({ required: true })
+  carrierId: string;
+
+  @ApiCarrierTimestamp({ required: true })
+  @Prop({ default: () => Date.now() })
+  timestamp: number;
+
+  @ApiProperty({
+    type: [Number, Number],
+    name: 'coordinates',
+    description:
+      'Koordinaten (longitude [-180 bis 180], latitude [-90 bis 90])',
+    example: [0, 0],
+    required: true,
+  })
+  @Prop({ required: true })
+  location: GeoJSON;
+
   @ApiCarrierLoad({ required: true })
   @Prop({ required: true })
   load: number;
